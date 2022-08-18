a107911c455db59bf6cede965397da6f8a7399bb
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-08-18 11:17:27 +0300

    first class function

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback){
+    callback();
+}
+
+function returnsANamedFunction(){
+    return function named(){
+
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return function (){
+        
+    }
+}
+
